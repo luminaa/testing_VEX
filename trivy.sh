bb3019a19d7c68f6fa08f48128eadcd19fc7305c
rahual rai <rairahual7@gmail.com>
2023-11-17 12:38:41 -0500

    debugged script file

--- trivy.sh
+++ trivy.sh
@@ -11,11 +11,11 @@ image=$2
 case "$format" in
     "table")
         output_file="trivy_${image//:/_}_${format}.txt"
-        trivy "$image" -o "$output_file" -f table
+        trivy image "$image" -o "$output_file" -f table
         ;;
     "cyclonedx")
         output_file="trivy_${image//:/_}_${format}.json"
-        trivy "$image" -o "$output_file" -f cyclonedx
+        trivy image "$image" -o "$output_file" -f cyclonedx
         ;;
     *)
         echo "Invalid format. Supported formats: table, cyclonedx"
